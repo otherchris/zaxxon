b72ee1f471c3dd4e8108aacde6fc41d2cf7e6ee3
Chris Caragianis <chriscaragianis@gmail.com>
2017-12-26 09:54:45 -0500

    rescaffold some tests, add report message

--- lib/zaxxon.ex
+++ lib/zaxxon.ex
@@ -42,29 +42,6 @@ defmodule Zaxxon do
     iex> Process.info(self(), :messages)
     {:messages, [{:sig, 1}]}
 
-    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> send(a, {:add_pid, b})
-    iex> send(a, {:sig, 1})
-    iex> send(b, {:sig, 1})
-    iex> Process.sleep(1)
-    iex> Process.info(self(), :messages)
-    {:messages, [{:sig, 1}, {:sig, 1}]}
-
-    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> send(a, {:del_pid, self()})
-    iex> send(a, {:sig, 1})
-    iex> Process.info(self(), :messages)
-    {:messages, []}
-
-    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> send(a, {:add_pid, b})
-    iex> send(a, {:del_pid, self()})
-    iex> send(a, {:sig, 1})
-    iex> Process.info(self(), :messages)
-    {:messages, []}
   """
   def neuron(a) do
     fn -> loop(a) end
@@ -84,16 +61,62 @@ defmodule Zaxxon do
     end
   end
 
+  @doc """
+  Add a pid to the outputs of a neuron
+
+  ## Examples
+
+    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    iex> send(a, {:add_pid, b})
+    iex> send(a, {:sig, 1})
+    iex> send(b, {:sig, 1})
+    iex> Process.sleep(1)
+    iex> Process.info(self(), :messages)
+    {:messages, [{:sig, 1}, {:sig, 1}]}
+
+  """
   def add_pid(pid, a = %{ c: c, t: t, next: pid_list}) do
     loop(%{c: c, t: t, next: pid_list ++ [pid]})
   end
 
+  @doc """
+  Remove a pid from the outputs of a neuron
+
+  ## Examples
+
+    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    iex> send(a, {:del_pid, self()})
+    iex> send(a, {:sig, 1})
+    iex> Process.info(self(), :messages)
+    {:messages, []}
+
+  """
   def del_pid(pid, a = %{ c: c, t: t, next: pid_list}) do
     loop(%{c: c, t: t, next: List.delete(pid_list, pid)})
   end
 
+  @doc """
+  Report the state of a neuron
+
+  ## Examples
+
+    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    iex> send(a, :report)
+    iex> Process.sleep(1)
+    iex> Process.info(self(), :messages)
+    {:messages, [{:report, %{ c: 0, t: 1, next: [self()] }}]}
+  """
+  def report(a, pid_list) do
+    Enum.each(pid_list, fn(x) -> send(x, {:report, a}) end)
+    loop(a)
+  end
+
   def loop(a = %{ c: c, t: t, next: pid_list }) when is_list(pid_list) do
     receive do
+      :tick -> process_sig(a)
+      :report -> report(a, pid_list)
       {:add_pid, pid} -> add_pid(pid, a)
       {:del_pid, pid} -> del_pid(pid, a)
       {:sig, sig} -> process_sig(%{c: c + sig, t: t, next: pid_list})
--- test/zaxxon_test.exs
+++ test/zaxxon_test.exs
@@ -2,4 +2,15 @@ defmodule ZaxxonTest do
   use ExUnit.Case
   doctest Zaxxon
 
+  test "add and remove pids" do
+    :flush
+    a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    send(a, {:add_pid, b})
+    send(a, {:del_pid, self()})
+    send(a, {:sig, 1})
+    Process.sleep(1)
+    assert Process.info(self(), :messages) == {:messages, []}
+  end
+
 end
