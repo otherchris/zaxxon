1eb572d554724c4616e552cabe1446a9722f2305
Chris Caragianis <chriscaragianis@gmail.com>
2017-12-26 15:10:02 -0500

    remove/add lists of pids

--- lib/zaxxon.ex
+++ lib/zaxxon.ex
@@ -2,6 +2,12 @@ defmodule Zaxxon do
   @moduledoc """
   Documentation for Zaxxon.
 
+  ASSUMPTIONS:
+
+    - Universal decay rate
+    - Decay only on message receipt
+    - Uniform distribution of charge in dentrites
+
   """
 
   @decay 0.05
@@ -20,7 +26,7 @@ defmodule Zaxxon do
     iex> send(a, {:sig, 1})
     iex> Process.sleep(1)
     iex> Process.info(self(), :messages)
-    {:messages, [{:sig, 1}]}
+    {:messages, [{:sig, 1.0}]}
 
     iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
     iex> send(a, {:sig, 0.1})
@@ -40,7 +46,7 @@ defmodule Zaxxon do
     iex> send(a, {:sig, 0.95})
     iex> Process.sleep(1)
     iex> Process.info(self(), :messages)
-    {:messages, [{:sig, 1}]}
+    {:messages, [{:sig, 1.0}]}
 
   """
   def neuron(a) do
@@ -51,7 +57,7 @@ defmodule Zaxxon do
     cond do
       c_new >= t ->
         Enum.each(pid_list, fn(x) ->
-          send(x, {:sig, t})
+          send(x, {:sig, t / length(pid_list)})
           loop(%{ c: 0, t: t, next: pid_list})
         end)
       c_new > 0 ->
@@ -73,9 +79,22 @@ defmodule Zaxxon do
     iex> send(b, {:sig, 1})
     iex> Process.sleep(1)
     iex> Process.info(self(), :messages)
-    {:messages, [{:sig, 1}, {:sig, 1}]}
+    {:messages, [{:sig, 0.5}, {:sig, 1.0}]}
+
+    iex> a = spawn Zaxxon.neuron(%{c: 0, t: 1, next: []})
+    iex> b = spawn Zaxxon.neuron(%{c: 0, t: 1, next: [self()]})
+    iex> send(a, {:add_pid, [b, self()]})
+    iex> send(a, :report)
+    iex> Process.sleep(1)
+    iex> {:messages, [{:report, %{c: 0, t: 1, next: n}}]} = Process.info(self(), :messages)
+    iex> length(n)
+    2
 
   """
+  def add_pid(pid, a = %{ c: c, t: t, next: pid_list}) when is_list(pid) do
+    loop(%{c: c, t: t, next: pid_list ++ pid})
+  end
+
   def add_pid(pid, a = %{ c: c, t: t, next: pid_list}) do
     loop(%{c: c, t: t, next: pid_list ++ [pid]})
   end
@@ -86,13 +105,26 @@ defmodule Zaxxon do
   ## Examples
 
     iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
-    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
     iex> send(a, {:del_pid, self()})
     iex> send(a, {:sig, 1})
+    iex> Process.sleep(1)
     iex> Process.info(self(), :messages)
     {:messages, []}
 
+    iex> a = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self()]  })
+    iex> b = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: []  })
+    iex> c = spawn Zaxxon.neuron(%{ c: 0, t: 1, next: [self(), a, b]  })
+    iex> send(c, {:del_pid, [a, b]})
+    iex> send(c, :report)
+    iex> Process.sleep(1)
+    iex> {:messages, [{:report, %{c: 0, t: 1, next: n}}]} = Process.info(self(), :messages)
+    iex> length(n)
+    1
   """
+  def del_pid(pid, a = %{ c: c, t: t, next: pid_list}) when is_list(pid) do
+    loop(%{c: c, t: t, next: pid_list -- pid})
+  end
+
   def del_pid(pid, a = %{ c: c, t: t, next: pid_list}) do
     loop(%{c: c, t: t, next: List.delete(pid_list, pid)})
   end
@@ -126,8 +158,9 @@ defmodule Zaxxon do
 
   def play do
     n = %{t: 1, c: 0, next: []}
-    ns = Enum.to_list(1..10) |> Enum.map(fn(x) -> spawn neuron(n) end)
-    IO.inspect ns
+    ns = Enum.to_list(1..10) |> Enum.map(fn(x) -> spawn neuron(n) end) |> Enum.with_index
+    nexts = Enum.to_list(1..10) |> Enum.map(fn(x) -> Enum.take_random(ns, 3) end) |> Enum.with_index
+    Enum.each(ns, fn({x, i}) -> send(x, {:add_pid, Enum.at(nexts, i)}) end)
   end
 
 end
